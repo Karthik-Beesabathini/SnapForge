ba9a8521fe4f2f94ebc784a5cd9022d656f566b1
Karthik B <karthikbeesabathini820@gmail.com>
2026-03-10 21:08:59 +0530

    College changed

--- camera.py
+++ camera.py
@@ -5,6 +5,30 @@ def capture_photos():
     cam = cv2.VideoCapture(0)
     photos = []
 
+    for i in range(6):
+
+        for j in range(3,0,-1):
+            ret, frame = cam.read()
+            temp = frame.copy()
+
+            cv2.putText(temp,str(j),(250,250),
+                        cv2.FONT_HERSHEY_SIMPLEX,7,(250,0,0),10)
+
+            cv2.imshow("Countdown",temp)
+            cv2.waitKey(1000)
+
+        ret, frame = cam.read()
+        photos.append(frame)
+        time.sleep(1)
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+    return photos
+def capture_photos1():
+    cam = cv2.VideoCapture(0)
+    photos1 = []
+
     for i in range(4):
 
         for j in range(3,0,-1):
@@ -18,10 +42,60 @@ def capture_photos():
             cv2.waitKey(1000)
 
         ret, frame = cam.read()
-        photos.append(frame)
+        photos1.append(frame)
         time.sleep(1)
 
     cam.release()
     cv2.destroyAllWindows()
 
-    return photos
+    return photos1
+def capture_photos2():
+    cam = cv2.VideoCapture(0)
+    photos2 = []
+
+    for i in range(3):
+
+        for j in range(3,0,-1):
+            ret, frame = cam.read()
+            temp = frame.copy()
+
+            cv2.putText(temp,str(j),(250,250),
+                        cv2.FONT_HERSHEY_SIMPLEX,7,(255,0,0),10)
+
+            cv2.imshow("Countdown",temp)
+            cv2.waitKey(1000)
+
+        ret, frame = cam.read()
+        photos2.append(frame)
+        time.sleep(1)
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+    return photos2
+def capture_photos3():
+    cam = cv2.VideoCapture(0)
+    photos2 = []
+
+    for i in range(2):
+
+        for j in range(3,0,-1):
+            ret, frame = cam.read()
+            temp = frame.copy()
+
+            cv2.putText(temp,str(j),(250,250),
+                        cv2.FONT_HERSHEY_SIMPLEX,7,(255,0,0),10)
+
+            cv2.imshow("Countdown",temp)
+            cv2.waitKey(1000)
+
+        ret, frame = cam.read()
+        photos2.append(frame)
+        time.sleep(1)
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+    return photos2
+
+
--- collage.py
+++ collage.py
@@ -4,8 +4,8 @@ import os
 
 def create_collage(photos):
 
-    collage=np.hstack((photos[0],photos[1]))
-    collage1=np.hstack((photos[2],photos[3]))
+    collage=np.hstack(((photos[0],photos[1],photos[2])))
+    collage1=np.hstack((photos[3],photos[4],photos[5]))
     collage2=np.vstack((collage,collage1))
 
     if not os.path.exists("collage"):
@@ -14,5 +14,43 @@ def create_collage(photos):
     path="collage/collages.jpg"
 
     cv2.imwrite(path,collage2)
+
+    return path
+def create_collage1(photos1):
+
+    collage=np.hstack((photos1[0],photos1[1]))
+    collage1=np.hstack((photos1[2],photos1[3]))
+    collage2=np.vstack((collage,collage1))
+
+    if not os.path.exists("collage"):
+        os.makedirs("collage")
+
+    path="collage/collages.jpg"
+
+    cv2.imwrite(path,collage2)
+
+    return path
+def create_collage2(photos2):
+
+    collage=np.hstack((photos2[0],photos2[1],photos2[2]))
+
+    if not os.path.exists("collage"):
+        os.makedirs("collage")
+
+    path="collage/collages.jpg"
+
+    cv2.imwrite(path,collage)
+
+    return path
+def create_collage3(photos3):
+
+    collage=np.hstack((photos3[0],photos3[1]))
+
+    if not os.path.exists("collage"):
+        os.makedirs("collage")
+
+    path="collage/collages.jpg"
+
+    cv2.imwrite(path,collage)
 
     return path
